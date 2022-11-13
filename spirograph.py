dfeaac437a2b9212dc9daed477616f771415b53a
Anyieth Aguek Ayuen <77279583+Aguek@users.noreply.github.com>
2022-11-14 01:07:23 +0300

    Update spirograph.py
    Dividing 360 by spaces because a circle has 360 degrees, and spaces determines the number of spaces between the circles of the spirograph . If they are 4, then 360/4 will = 90, which means there will be 90 circles, hence making it loop for as long as necessary.
    Putting everything in a function

--- spirograph.py
+++ spirograph.py
@@ -15,9 +15,13 @@ def random_color():
 
 turtle.speed("fastest")
 turtle.pensize(3)
-for _ in range(320):
-    turtle.color(random_color())
-    turtle.circle(120)
-    turtle.left(10)
+def draw_spirograph(spaces):
+    #dividing 360 by spaces because a circle has 360 degrees, and spaces determines the number of spaces between the circles of the spirograph
+    #if they are 4, then 360/4 will = 90, which means there will be 90 circles, hence making it loop for as long as necessary
+    for _ in range(int(360 / spaces)):
+        turtle.color(random_color())
+        turtle.circle(120)
+        turtle.setheading(turtle.heading() + spaces)
 
+draw_spirograph(4)
 screen.exitonclick()
